a794e42382d355be5e3186d1554d79ea998b07df
Majed <majed79a@gmail.com>
2023-05-28 18:47:54 +0330

    wired up test site for results

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinLengthValidator(1),MaxLengthValidator(5)])
+    rating = models.IntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
 
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
+
+def index(requst):
+    books = Book.objects.all()
+    return render(requst, "book_outlet/index.html", {
+        "books": books
+    })
