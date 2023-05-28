219beeeffd01bee72fd657114b5faa698a09ed8e
Majed <majed79a@gmail.com>
2023-05-28 22:21:52 +0330

    added detail page for Books

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Book
 
@@ -8,4 +9,16 @@ def index(requst):
     books = Book.objects.all()
     return render(requst, "book_outlet/index.html", {
         "books": books
+    })
+def book_detail(requset, id):
+    #try:
+    #    book = Book.objects.get(pk=id)
+    #except:
+    #    raise Http404()
+    book = get_object_or_404(Book, pk=id)
+    return render(requset, "book_outlet/bookdetail.html",{
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
     })
