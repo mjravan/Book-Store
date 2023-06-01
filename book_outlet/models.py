2d40cd933fe1ea25d6a266a47c38f20baa385178
Majed <majed79a@gmail.com>
2023-06-01 23:11:52 +0330

    added slug field, aggregation and ordering queries

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.core import validators
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.urls import reverse
+from django.utils.text import slugify
+
 
 # Create your models here.
 
@@ -8,6 +12,14 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)  # Harry Potter 1 => harry-potter-1
+
+    def get_absolute_path(self):
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+# from django.http import Http404
+from django.db.models import Avg
+
 
 from .models import Book
 
+
 # Create your views here.
 
-def index(requst):
-    books = Book.objects.all()
-    return render(requst, "book_outlet/index.html", {
-        "books": books
+def index(request):
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_numbers_of_books": num_books,
+        "average_rating": avg_rating
     })
-def book_detail(requset, id):
-    #try:
+
+
+def book_detail(request, slug):
+    # try:
     #    book = Book.objects.get(pk=id)
-    #except:
+    # except:
     #    raise Http404()
-    book = get_object_or_404(Book, pk=id)
-    return render(requset, "book_outlet/bookdetail.html",{
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, "book_outlet/bookdetail.html", {
         "title": book.title,
         "author": book.author,
         "rating": book.rating,
